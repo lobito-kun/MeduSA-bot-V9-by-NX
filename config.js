f5f3f315992399c92c245547d381cc6cabd75564
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-10-25 21:12:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-global.owner = ['5493546431050', '56935558580', '5493888629737', '56937489312'] // Put your number here
-global.smod = ['573245902682', '50245893464'] // Want some help?
-global.mods = ['573245902682', '5493546431050', '50375337360', '5491140945882', '5491131916269', '50245893464', '5491133099333'] // Want some help?
-global.prems = ['50582818291', '5219384054782', '5491131916269', '5493795022497', '573103071789', '50583318333'] // Premium user has unlimited limit
+global.owner = ['5493704583368'] // Put your number here
+global.smod = [''] // Want some help?
+global.mods = [''] // Want some help?
+global.prems = ['5493704583368'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
    xteam: 'https://api.xteam.xyz',
